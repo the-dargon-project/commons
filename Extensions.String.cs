41efd2188a7dde1fc7ce1a732289d1c68da98b6e
Michael Yu <ItzWarty@gmail.com>
2014-07-04 00:22:31 -0700

    Fixed .MltPad0 removals

--- Extensions.String.cs
+++ Extensions.String.cs
@@ -14,11 +14,11 @@ namespace ItzWarty
       public static string ToHttpTimestamp(this DateTime dt)
       {
          DateTime utc = dt.ToUniversalTime();
-         string day = utc.Day.ToString().MltPad0();
+         string day = utc.Day.ToString().PadLeft(2, '0');
          string dayOfWeek = utc.DayOfWeek.ToString().Substring(0, 3);
          string year = utc.Year.ToString();
          string mon = new string[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" }[utc.Month - 1];
-         return dayOfWeek + ", " + day + " " + mon + " " + year + " " + utc.Hour.ToString().MltPad0() + ":" + utc.Minute.ToString().MltPad0() + ":" + utc.Second.ToString().MltPad0() + " GMT";
+         return dayOfWeek + ", " + day + " " + mon + " " + year + " " + utc.Hour.ToString().PadLeft(2, '0') + ":" + utc.Minute.ToString().PadLeft(2, '0') + ":" + utc.Second.ToString().PadLeft(2, '0') + " GMT";
       }
 
       /// <summary>
