4e48c49c1be3e8cb83ac6d656cd12ff695c7ca21
Michael Yu <ItzWarty@gmail.com>
2014-08-29 00:15:37 -0700

    Extensions: Added Collections.Get, Collections.ForEach.

--- Extensions.Collections.cs
+++ Extensions.Collections.cs
@@ -9,6 +9,13 @@ namespace ItzWarty
 {
    public static partial class Extensions
    {
+      public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+      {
+         foreach (var element in enumerable) {
+            action(element);
+         }
+      }
+
       /// <summary>
       /// Gets a subarray of the given array
       /// http://stackoverflow.com/questions/943635/c-arrays-getting-a-sub-array-from-an-existing-array
@@ -21,6 +28,15 @@ namespace ItzWarty
          Array.Copy(data, index, result, 0, length);
          return result;
       }
+
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      public static T Get<T>(this T[] collection, int index) { return collection[index]; }
+
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      public static T Get<T>(this IList<T> collection, int index) { return collection[index]; }
+
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      public static V Get<K, V>(this IDictionary<K, V> dict, K key) { return dict[key]; }
 
       [MethodImpl(MethodImplOptions.AggressiveInlining)]
       public static V GetValueOrDefault<K, V>(this Dictionary<K, V> dict, K key)
