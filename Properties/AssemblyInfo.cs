a005196a7c9fa8b8c9f7c0656f3a541934847c68
Michael Yu <ItzWarty@gmail.com>
2014-11-16 12:15:07 -0800

    Cleanup AssemblyInfo. Apparently an empty <Description></Description> breaks nuget pack.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ItzWartyLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("libwarty")]
+[assembly: AssemblyDescription("Various utilities and extension methods to make your life easier.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ItzWarty")]
-[assembly: AssemblyProduct("ItzWartyLib")]
-[assembly: AssemblyCopyright("Copyright © ItzWarty 2012")]
+[assembly: AssemblyProduct("libwarty")]
+[assembly: AssemblyCopyright("Copyright © ItzWarty 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
