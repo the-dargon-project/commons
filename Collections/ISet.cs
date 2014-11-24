ab66f6087308be90007929f339915c6515556ddf
Michael Yu <ItzWarty@gmail.com>
2014-11-23 21:33:55 -0800

    Continue to fix overload resolution issues - ISet has 'new' implementation for intersection between ICollection<T> and IReadOnlySet<T>.

--- Collections/ISet.cs
+++ Collections/ISet.cs
@@ -3,5 +3,7 @@ using System.Collections.Generic;
 
 namespace ItzWarty.Collections {
    public interface ISet<T> : ICollection<T>, IEnumerable<T>, IEnumerable, IReadOnlySet<T> {
+      new int Count { get; }
+      new void CopyTo(T[] array, int arrayIndex);
    }
 }
