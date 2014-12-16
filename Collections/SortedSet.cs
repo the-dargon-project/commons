081ab971bf24480e620a5e59fbb244754e39389e
Michael Yu <ItzWarty@gmail.com>
2014-12-16 03:26:43 -0800

    Make SortedSet public.

--- Collections/SortedSet.cs
+++ Collections/SortedSet.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 
 namespace ItzWarty.Collections {
-   class SortedSet<T> : System.Collections.Generic.SortedSet<T>, ISortedSet<T> {
+   public class SortedSet<T> : System.Collections.Generic.SortedSet<T>, ISortedSet<T> {
       public SortedSet() { } 
       public SortedSet(IComparer<T> comparer) : base(comparer) { } 
       public SortedSet(IEnumerable<T> collection) : base(collection) { }
