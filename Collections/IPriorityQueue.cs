512144daf27778df9fd91a342b4fd86dcde3c7f1
Michael Yu <ItzWarty@gmail.com>
2015-02-15 02:29:40 -0800

    Resolve ambiguities in Priority Queue interface implementation.

--- Collections/IPriorityQueue.cs
+++ Collections/IPriorityQueue.cs
@@ -2,28 +2,14 @@
 
 namespace ItzWarty.Collections {
    public interface IPriorityQueue<T> : IQueue<T> where T : IComparable<T> {
-      /// <summary>Clear all the elements from the priority queue</summary>
-      void Clear();
-
       /// <summary>Add an element to the priority queue - O(log(n)) time operation.</summary>
       /// <param name="item">The item to be added to the queue</param>
       void Add(T item);
-
-      /// <summary>Returns the number of elements in the queue.</summary>
-      int Count { get; }
 
       /// <summary>Returns true if the queue is empty.</summary>
       /// Trying to call Peek() or Next() on an empty queue will throw an exception.
       /// Check using Empty first before calling these methods.
       bool Empty { get; }
-
-      /// <summary>Allows you to look at the first element waiting in the queue, without removing it.</summary>
-      /// This element will be the one that will be returned if you subsequently call Next().
-      T Peek();
-
-      /// <summary>Removes and returns the first element from the queue (least element)</summary>
-      /// <returns>The first element in the queue, in ascending order.</returns>
-      T Dequeue();
 
       bool Remove(T value);
    }
---
+++ Tests/Collections/PriorityQueueUsabilityTest.cs
@@ -0,0 +1,33 @@
+﻿using NMockito;
+using Xunit;
+using ICL = ItzWarty.Collections;
+using SCG = System.Collections.Generic;
+
+namespace ItzWarty.Collections {
+   public class PriorityQueueUsabilityTest : NMockitoInstance {
+      private readonly ICL.PriorityQueue<int> testObj = new PriorityQueue<int>().With(x => {
+         x.Add(1);
+         x.Add(2);
+      });
+
+      [Fact]
+      public void ClearLacksAmbiguity() {
+         testObj.Clear();
+      }
+
+      [Fact]
+      public void CountLacksAmbiguity() {
+         AssertEquals(2, testObj.Count);
+      }
+
+      [Fact]
+      public void PeekLacksAmbiguity() {
+         AssertEquals(1, testObj.Peek());
+      }
+
+      [Fact]
+      public void DequeueLacksAmbiguity() {
+         AssertEquals(1, testObj.Dequeue());
+      }
+   }
+}
