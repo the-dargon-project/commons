27939e5fc387e2bfb326d73a7bd4e538684f13a3
Michael Yu <ItzWarty@gmail.com>
2015-08-01 17:33:52 -0700

    Added Ryu Package for ItzWarty.Commons.

---
+++ ItzWartyCommonsRyuPackage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dargon.Ryu;
+using ItzWarty.Collections;
+using ItzWarty.Pooling;
+
+namespace ItzWarty {
+   public class ItzWartyCommonsRyuPackage : RyuPackageV1 {
+      public ItzWartyCommonsRyuPackage() {
+         Singleton<ICollectionFactory, CollectionFactory>();
+         Singleton<ObjectPoolFactory, DefaultObjectPoolFactory>();
+      }
+   }
+}
