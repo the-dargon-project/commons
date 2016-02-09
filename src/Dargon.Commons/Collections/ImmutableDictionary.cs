6fd4cdb241a3c996d0dea57bc49d350a18e1b235
Warty <ItzWarty@gmail.com>
2016-02-08 20:40:22 -0800

    Cleanup immutabledictionary newlines and braces

--- src/Dargon.Commons/Collections/ImmutableDictionary.cs
+++ src/Dargon.Commons/Collections/ImmutableDictionary.cs
@@ -20,8 +20,7 @@ namespace Dargon.Commons.Collections {
     * }
     * result
     */
-   public static class ImmutableDictionary
-   {
+   public static class ImmutableDictionary {
       public static IReadOnlyDictionary<K, V> Of<K, V>() { return new ListDictionary<K, V> { }; }
       public static IReadOnlyDictionary<K, V> Of<K, V>(K k1, V v1) { return new ListDictionary<K, V> {[k1] = v1 }; }
       public static IReadOnlyDictionary<K, V> Of<K, V>(K k1, V v1, K k2, V v2) { return new ListDictionary<K, V> {[k1] = v1,[k2] = v2 }; }
@@ -32,5 +31,6 @@ namespace Dargon.Commons.Collections {
       public static IReadOnlyDictionary<K, V> Of<K, V>(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7) { return new ListDictionary<K, V> {[k1] = v1,[k2] = v2,[k3] = v3,[k4] = v4,[k5] = v5,[k6] = v6,[k7] = v7 }; }
       public static IReadOnlyDictionary<K, V> Of<K, V>(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8) { return new ListDictionary<K, V> {[k1] = v1,[k2] = v2,[k3] = v3,[k4] = v4,[k5] = v5,[k6] = v6,[k7] = v7,[k8] = v8 }; }
       public static IReadOnlyDictionary<K, V> Of<K, V>(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9) { return new ListDictionary<K, V> {[k1] = v1,[k2] = v2,[k3] = v3,[k4] = v4,[k5] = v5,[k6] = v6,[k7] = v7,[k8] = v8,[k9] = v9 }; }
-      public static IReadOnlyDictionary<K, V> Of<K, V>(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10) { return new ListDictionary<K, V> {[k1] = v1,[k2] = v2,[k3] = v3,[k4] = v4,[k5] = v5,[k6] = v6,[k7] = v7,[k8] = v8,[k9] = v9,[k10] = v10 }; }   }
+      public static IReadOnlyDictionary<K, V> Of<K, V>(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10) { return new ListDictionary<K, V> {[k1] = v1,[k2] = v2,[k3] = v3,[k4] = v4,[k5] = v5,[k6] = v6,[k7] = v7,[k8] = v8,[k9] = v9,[k10] = v10 }; }
+   }
 }
