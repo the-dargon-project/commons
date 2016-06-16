43bf5244d83e4507f5acf47be7dcc258674e1152
Warty <ItzWarty@gmail.com>
2016-06-15 23:06:33 -0700

    ISE supports message

--- src/Dargon.Commons/Exceptions/InvalidStateException.cs
+++ src/Dargon.Commons/Exceptions/InvalidStateException.cs
@@ -4,5 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Dargon.Commons.Exceptions {
-   public class InvalidStateException : Exception {}
+   public class InvalidStateException : Exception {
+      public InvalidStateException() : base() { }
+      public InvalidStateException(string message) : base(message) { }
+   }
 }
